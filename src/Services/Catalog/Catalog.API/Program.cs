9d2e9eb223fd605fdcddb07f3a5225985373a3c8
NasirIslam <nasir.islam1983@gmail.com>
2024-11-16 17:37:37 +0400

    last changes

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -2,6 +2,11 @@ var builder = WebApplication.CreateBuilder(args);
 //Add the Services to the Container
 builder.Services.AddCarter();
 builder.Services.AddMediatR(config => config.RegisterServicesFromAssembly(typeof (Program).Assembly));
+builder.Services.AddMarten(opt =>
+{
+    opt.Connection(builder.Configuration.GetConnectionString("database")!);
+}
+).UseLightweightSessions();
 
 var app = builder.Build();
 //Configure the HTTPS request pipeline
