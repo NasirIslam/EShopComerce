ba588814f50bd2c0bc10f009a183c07fb7138eb8
NasirIslam <nasir.islam1983@gmail.com>
2025-01-12 12:48:01 +0400

    Add new Exception Class

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -5,6 +5,16 @@ namespace Catalog.API.Products.UpdateProduct
 {
     public record UpdateProductCommand(Guid Id, string Name, List<string> Category, string Description, decimal Price, int Stock) : ICommand<UpdateProductResult> ;
     public record UpdateProductResult(bool IsSuccess);
+
+    public class UpdateProductCommandValidator:AbstractValidator<UpdateProductCommand>
+    {
+        public UpdateProductCommandValidator() {
+            RuleFor(c => c.Id).NotEmpty().WithMessage("Id is mandatory");
+            RuleFor(c => c.Name).NotEmpty().WithMessage("Name is mandatory").Length(2,100).WithMessage("Length should be greater than 2 and less than 100");
+            RuleFor(c => c.Price).GreaterThan(0).WithMessage("Price should be greater than zero");
+        }
+        
+    }
     internal  class UpdateProducCommandtHandler(IDocumentSession session,ILogger<UpdateProducCommandtHandler> logger) : ICommandHandler<UpdateProductCommand, UpdateProductResult>
     {
         public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,4 +1,8 @@
 using BuildingBlocks.Behaviours;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using OpenTelemetry.Trace;
+using static System.Net.Mime.MediaTypeNames;
 
 var builder = WebApplication.CreateBuilder(args);
 //Add the Services to the Container
@@ -8,9 +12,7 @@ builder.Services.AddMediatR(
     {
         config.RegisterServicesFromAssembly(typeof(Program).Assembly);
         config.AddOpenBehavior(typeof(ValidationBehaviours<,>));
-
     }
-
     );
 builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
 builder.Services.AddMarten(opt =>
@@ -22,6 +24,29 @@ builder.Services.AddMarten(opt =>
 var app = builder.Build();
 //Configure the HTTPS request pipeline
 app.MapCarter();
+app.UseExceptionHandler(exceptionHandlerApp =>
+{
+    exceptionHandlerApp.Run(async context =>
+    {
+        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+        if (exception == null)
+            return;
+        var problemDetails = new ProblemDetails
+        {
+            Title = exception.Message,
+            Status = StatusCodes.Status500InternalServerError,
+            Detail = exception.StackTrace
+        };
+        var logger = context.RequestServices.GetService<ILogger<Program>>();
+        logger.LogError(exception, exception.Message);
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Response.ContentType = "application/problem+json";
+        await context.Response.WriteAsJsonAsync(problemDetails);
+
+
+    });
+});
+
 
 
 app.Run();
